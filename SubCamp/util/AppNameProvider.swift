5beb39365a3f7f34b201c0a671cbe90d43c7cdeb
Benjamin <22482064+bsurrey@users.noreply.github.com>
2024-04-04 03:00:03 +0200

    bug fixes

--- SubCamp/util/AppNameProvider.swift
+++ SubCamp/util/AppNameProvider.swift
@@ -2,17 +2,17 @@
 //  AppNameProvider.swift
 //  SubCamp
 //
-//  Created by Benjamin Surrey on 03.04.24.
+//  Created by Benjamin Surrey on 04.04.24.
 //
 
 import Foundation
 
 enum AppNameProvider {
     static func appName(in bundle: Bundle = .main) -> String {
-        guard let version = bundle.object(forInfoDictionaryKey: "CFBundleName") as? String else {
+        guard let name = bundle.object(forInfoDictionaryKey: "CFBundleName") as? String else {
             fatalError("CFBundleName should not be missing from info dictionary")
             
         }
-        return version
+        return name
     }
 }
