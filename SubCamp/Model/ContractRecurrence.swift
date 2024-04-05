b9f4730bf83a2870e827f2038cabd684a4b2ea20
Benjamin <22482064+bsurrey@users.noreply.github.com>
2024-04-06 01:04:44 +0200

    bug fixes and refinements

---
+++ SubCamp/Model/ContractRecurrence.swift
@@ -0,0 +1,20 @@
+//
+//  ContractRepeater.swift
+//  SubCamp
+//
+//  Created by Benjamin Surrey on 06.04.24.
+//
+
+import Foundation
+import SwiftData
+import SwiftUI
+
+enum ContractRepeater: Codable {
+    case daily
+    case weekly
+    case biweekly
+    case monthly
+    case quarterly
+    case halfyerly
+    case yearly
+}
---
+++ SubCamp/View/styles/ColorfulIconLabelStyle.swift
@@ -0,0 +1,8 @@
+//
+//  ColorfulIconLabelStyle.swift
+//  SubCamp
+//
+//  Created by Benjamin Surrey on 06.04.24.
+//
+
+import Foundation
